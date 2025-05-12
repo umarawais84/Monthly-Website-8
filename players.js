ca9a3b32974bcfb8d097db0e069b7b5a72c2b387
umarawais84 <writetoumarawais@gmail.com>
2025-05-12 10:42:00 -0400

    Day 1

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+  {
+    firstName: "Francisco",
+    lastName: "Lindor",
+    position: "SS",
+    age: 31,
+    photo: "https://picsum.photos/200/300",
+  },
+  {
+    firstName: "Francisco",
+    lastName: "Lindor",
+    position: "SS",
+    age: 31,
+    photo: "https://picsum.photos/200/300",
+  },
+  {
+    firstName: "Francisco",
+    lastName: "Lindor",
+    position: "SS",
+    age: 31,
+    photo: "https://picsum.photos/200/300",
+  },
+  {
+    firstName: "Francisco",
+    lastName: "Lindor",
+    position: "SS",
+    age: 31,
+    photo: "https://picsum.photos/200/300",
+  },
+  {
+    firstName: "Francisco",
+    lastName: "Lindor",
+    position: "SS",
+    age: 31,
+    photo: "https://picsum.photos/200/300",
+  },
+];
