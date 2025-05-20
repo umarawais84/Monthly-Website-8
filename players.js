a03989d866843525d7bb0f92dee805e8984e5f53
umarawais84 <99414210+umarawais84@users.noreply.github.com>
2025-05-20 13:26:33 +0000

    imgs

--- players.js
+++ players.js
@@ -4,34 +4,37 @@ const players = [
     lastName: "Awais",
     position: "5K",
     age: 16,
-    photo: "imgs/umar.jpeg",
+    photo_1: "imgs/umar.jpeg",
+    photo_2: "imgs/umar-l.jpeg"
   },
   {
     firstName: "Wyatt",
     lastName: "Bedor",
     position: "800",
     age: 16,
-    photo: "imgs/wyatt.jpeg",
+    photo_1: "imgs/wyatt.jpeg",
+    photo_2: "imgs/wyatt-l.jpeg"
   },
   {
     firstName: "Richard",
     lastName: "Riopel",
     position: "1600",
     age: 16,
-    photo: "imgs/richard.jpeg",
+    photo_1: "imgs/richard.jpeg",
+    photo_2: "imgs/richard-1.jpeg"
   },
   {
-    firstName: "Joseph",
-    lastName: "Friedman",
+    firstName: "Matthew",
+    lastName: "Steinway",
     position: "1600",
-    age: 16,
-    photo: "https://picsum.photos/200/300",
+    age: 15,
+    photo_1: "https://picsum.photos/200/300",
   },
   {
     firstName: "Rohan",
     lastName: "Shah",
     position: "3200",
     age: 17,
-    photo: "https://picsum.photos/200/300",
+    photo_1: "https://picsum.photos/200/300",
   },
 ];
--- roster.js
+++ roster.js
@@ -10,14 +10,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
             col.innerHTML = `
                 <div class="card h-100 shadow-sm">
-                    <img src="${p.photo}" alt="${p.firstName} ${p.lastName}" class="card-img-top">
+                <div class="carousel-item active">
+      <img class="d-block w-100" src="${p.photo_1}" alt="${p.firstName} ${p.lastName}">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
                     <div class="card-body text-center">
                         <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                        <div class="badge badge-position badge-pos-${p.position}">${p.position}</div>
+                        <div class="text-primary badge badge-position badge-pos-${p.position}">${p.position}</div>
                         <p class="small text-muted mb-0">Age ${p.age}</p>
                         <button type="button" class="btn btn-primary">Click Me!</button>
                     </div>
-                </div>        
+                </div>
             `
             grid.appendChild(col)
         })
